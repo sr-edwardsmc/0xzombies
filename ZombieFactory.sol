6e99d4b53dc357f8426fa536898b801b3ce521ff
Edward Monsalve Castrillon <edward.monsalvec@gmail.com>
2025-01-25 20:36:24 -0500

    feat: add mapping for zombies indetification and limit to allow only 1 zombie per address

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -14,9 +14,14 @@ contract ZombieFactory {
 
   Zombie[] public zombies;
 
+  mapping (uint => address) public zombieToOwner;
+  mapping (address => uint) ownerZombieCount;
+
   function _createZombie(string memory _name, uint _dna) private {
     zombies.push(Zombie(_name, _dna));
     uint id = zombies.length - 1;
+    zombieToOwner[id] = msg.sender;
+    ownerZombieCount[msg.sender]++;
     emit NewZombie(id, _name, _dna);
   }
 
@@ -26,9 +31,9 @@ contract ZombieFactory {
   }
 
   function createRandomZombie(string memory _name) public {
+    require(ownerZombieCount[msg.sender] == 0, "You already have a zombie");
     uint randDna = _generateRandomDna(_name);
     _createZombie(_name, randDna);
   }
-
 
 }
