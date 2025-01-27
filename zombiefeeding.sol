be8c55759e444c9f78b25197a786da7e512a03da
Edward Monsalve Castrillon <edward.monsalvec@gmail.com>
2025-01-27 17:16:23 -0500

    feat: add zombies helper functionalities and modifiers. Added cooldown time after feeding

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -22,15 +22,25 @@ contract ZombieFeeding is ZombieFactory {
   
   KittyInterface kittyContract;
 
-  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
+    require(_isReady(myZombie));
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
     if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
       newDna = newDna - newDna % 100 + 99;
     }
     _createZombie("NoName", newDna);
+    _triggerCooldown(myZombie);
+  }
+
+  function _triggerCooldown(Zombie storage _zombie) internal {
+    _zombie.readyTime = uint32(now + cooldownTime);
+  }
+
+  function _isReady(Zombie storage _zombie) internal view returns (bool) {
+    return (_zombie.readyTime <= now);
   }
 
   function feedOnKitty(uint _zombieId, uint _kittyId) public {
@@ -39,7 +49,7 @@ contract ZombieFeeding is ZombieFactory {
     feedAndMultiply(_zombieId, kittyDna, "kitty");
   }
 
-  function setKittyAddress(address _address) external onlyOwner {
+  function setKittyContractAddress(address _address) external onlyOwner {
     kittyContract = KittyInterface(_address);
   }
 
