0f07eba4faa2f21ffcd5a5075317e2ed2e97b4cb
Edward Monsalve Castrillon <edward.monsalvec@gmail.com>
2025-01-30 15:09:36 -0500

    feat: add payable function to levelUp zombies, add withraw function

---
+++ zombieattack.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.8.0 <0.9.0;
+
+import "./zombiehelper.sol";
+
+contract ZombieAttack is ZombieHelper {
+
+  
+
+}
--- zombiehelper.sol
+++ zombiehelper.sol
@@ -9,6 +9,8 @@ contract ZombieHelper is ZombieFeeding {
     _;
   }
 
+  uint levelAppFee = 0.001 ether;
+
   function changeName(uint _zombieId, string calldata _newName) external aboveLevet(2, _zombieId) {
     require(msg.sender == zombieToOwner[_zombieId]);
     zombies[_zombieId].name = _newName;
@@ -17,5 +19,27 @@ contract ZombieHelper is ZombieFeeding {
   function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
     require(msg.sender == zombieToOwner[_zombieId]);
     zombies[_zombieId].dna = _newDna;
+  }
+
+  function withdraw() external onlyOwner {
+    address payable _owner = payable(owner());
+    _owner.transfer(address(this).balance);
+  }
+
+  function levelUp(uint _zombieId) external payable {
+    require(msg.value == levelAppFee);
+    zombies[_zombieId].level++;
+  }
+
+  function getZombiesByOwner(address _owner) external view returns (uint[] memory) {
+    uint[] memory result = new uint[](ownerZombieCount[_owner]);
+    uint counter = 0;
+    for (uint i = 0; i < zombies.length; i++) {
+      if (zombieToOwner[i] == _owner) {
+        result[counter] = i;
+        counter++;
+      }
+    }
+    return result;
   }
 }
